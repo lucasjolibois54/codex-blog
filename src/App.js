08aeb8b508abc675597515ffd5c8c9cbc3a0ad0f
lucasjolibois54 <lucasjolibois@icloud.com>
2022-05-09 14:35:11 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import AllPosts from "./components/AllPosts.js";
 import OnePost from "./components/OnePost.js";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <Route component={AllPosts} path="/" exact />
-        <Route component={OnePost} path="/onepost" />
-      </div>
-    </BrowserRouter>
+    <div>
+      <h1>et eller andet</h1>
+    <Routes>
+      <Route exact path="/" element={<AllPosts/>} />
+        <Route path="/onepost" element={<OnePost/>}/>
+      
+    </Routes>
+    </div>
   );
 }
 export default App;
